857aa40a131b06bc7e3fbf85b81c8a699f01a41c
RomaKis <romaikiss@gmail.com>
2018-04-18 22:15:51 +0300

    2: hello world implementation

---
+++ Finance/Block/Finance.php
@@ -0,0 +1,12 @@
+<?php
+namespace SweetHome\Finance\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class Finance extends Template
+{
+    public function getHelloWorldTxt()
+    {
+        return 'Hello world!';
+    }
+}
